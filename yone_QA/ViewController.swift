d5c7171569b8cb65008e1f6480b6e51dafa7bf19
andy <a5706617@gmail.com>
2021-12-30 19:20:52 +0800

    Initial Commit

---
+++ yone_QA/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  yone_QA
+//
+//  Created by 江啟綸 on 2021/12/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
